f48fca72c533c0e66866c31c65516ff3e9b75181
Nicolás Mella <nicolasmella2004@gmail.com>
2025-05-16 14:46:34 -0400

    feat: add pages bases

--- src/pages/Backlog.jsx
+++ src/pages/Backlog.jsx
@@ -1,5 +1,12 @@
 function Backlog() {
+    const project = "Proyecto #1"
 
+    return (
+        <>
+            <h1 className="display-4 fw-bold text-center mb-3">Product Backlog 📋</h1>
+            <p className="fs-4 fw-lighter text-center mb-4">{project}</p>
+        </>
+      )
 }
 
 export default Backlog
--- src/pages/Sprints.jsx
+++ src/pages/Sprints.jsx
@@ -1,5 +1,12 @@
 function Sprints() {
-
+    const project = "Proyecto #1"
+    
+    return (
+        <>
+            <h1 className="display-4 fw-bold text-center mb-3">Sprints 📅</h1>
+            <p className="fs-4 fw-lighter text-center mb-4">{project}</p>
+        </>
+      )
 }
 
 export default Sprints
